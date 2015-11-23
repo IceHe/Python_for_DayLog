057ccfca6a38df070c0f89e296cb40340fa11141
IceHe_何志远 <ice.he.hzy@gmail.com>
2015-11-23 10:24:57 +0800

    Added Resort_Weeks_Log_Id_in_DB.py

--- Not_Often/Resort_Weeks_Log_Id_in_DB.py
+++ Not_Often/Resort_Weeks_Log_Id_in_DB.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     conn = mysql.connector.connect(user = 'test', password = '88887777', database='life_log')
     cursor = conn.cursor()
 
-    sql = r'select id, date_ymd from day_log order by date_ymd asc'
+    sql = r'select id, from_date from week_log order by from_date asc'
     cursor.execute(sql)
     days = cursor.fetchall()
     print(days)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     tmp_id_start = 1000
     for d in days:
         # print(d[0])
-        sql = r'update day_log set id = "' + str(tmp_id_start) + r'" where id = "' + str(d[0]) + '"'
+        sql = r'update week_log set id = "' + str(tmp_id_start) + r'" where id = "' + str(d[0]) + '"'
         print(sql)
         cursor.execute(sql)
         tmp_id_start += 1
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     tmp_id_start = 1000
     new_id_start = 1
     for d in days:
-        sql = r'update day_log set id = "' + str(new_id_start) + r'" where id = "' + str(tmp_id_start) + '"'
+        sql = r'update week_log set id = "' + str(new_id_start) + r'" where id = "' + str(tmp_id_start) + '"'
         print(sql)
         cursor.execute(sql)
         new_id_start += 1
